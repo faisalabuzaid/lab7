b28d69e403c3328e1ec68dd04435b5f7f6ca9ea3
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-03-29 22:22:59 +0300

    finishweather

---
+++ README.md
@@ -0,0 +1,22 @@
+# city_explorer_api
+
+**Author**: Faisal Abuzaid
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+An application for travelers that let the user know specefic informations about cities and near by resturants for each city
+## Getting Started
+1. Build the front-end application
+2. build the back-end
+3. connect them together
+4. add a dataBase
+## Architecture
+City explorer was designed by HTM, CSS JavaScript and Node.js
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
+
+28-03-2021 05:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
+
+## Credits and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
---
+++ index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+
+
+app.get('/location', handleLocation);
+
+app.get('/weather', handleWeather)
+
+
+let myLocalLocations ={};
+function handleLocation(request, response) {
+  let city = request.query.city;
+  if (myLocalLocations[city]) {
+    response.send(myLocalLocations[city]);
+  } else {
+    let key = process.env.GEO_API_KEY;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+    superagent.get(url).then(res=> {
+      const locationData = res.body[0];
+      const location = new Location(city, locationData);
+      myLocalLocations[city] = location;
+      response.send(location);
+
+    }).catch((err)=> {
+      console.log('ERROR IN LOCATION API');
+      console.log(err)
+    });
+  }
+}
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData.display_name;
+  this.latitude = geoData.lat;
+  this.longitude = geoData.lon;
+}
+
+
+let myLocalWeather = {};
+function handleWeather(request, response) {
+  let city = request.query.city;
+  if (myLocalWeather[city]) {
+    response.send(myLocalWeather[city]);
+  } else {
+    let key = process.env.WETH_API_KEY;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`
+    superagent.get(url).then(res => {
+      console.log(res.body.data);
+      const data = res.body.data.map((wData) => {
+        return new Weather(wData.weather.description, wData.valid_date);
+        console.log(data);
+      });
+      for (let i = 0; i < 8; i++) {
+        data.pop();
+      }
+      response.send(data);
+      myLocalWeather[city] = data;
+    })
+  }
+}
+
+//   const weather = require('./data/weather.json');
+//   const weatherData = weather.data;
+//   let weatherArr = [];
+//   weatherData.forEach(item => {
+//     let obj = {
+//       'forecast': item.weather.description,
+//       'time' : item.valid_date,
+//     };
+//     weatherArr.push(obj);
+//   })
+
+//   response.send(weatherArr);
+// }
+
+function Weather(forecast, datetime) {
+  this.forecast = forecast;
+  this.datetime = datetime;
+}
+
+
+app.listen(PORT, ()=> console.log(`App is running on Server on port: ${PORT}`))
